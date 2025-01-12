9a8e0b9302f4b3d152f4cf1686cbe01ba1f0e329
Paulo Henrique <pauloh1288@gmail.com>
2025-01-12 13:11:59 -0300

    refactor: improve var name and reorganize imports

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-
 import preact from "@astrojs/preact";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
