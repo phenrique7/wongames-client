c96f5aeac8f9be33c440a1dff37d84ea679ab3ed
Paulo Henrique <pauloh1288@gmail.com>
2025-02-01 18:35:20 -0300

    feat: implement base auth forms

---
+++ src/ui/common/button/button.sva.ts
@@ -0,0 +1,97 @@
+import { sva } from "../../../../styled-system/css";
+
+export const buttonSva = sva({
+  slots: ["root", "icon"],
+  base: {
+    root: {
+      width: "100%",
+      border: "none",
+      cursor: "pointer",
+      borderRadius: "sm",
+      alignItems: "center",
+      whiteSpace: "nowrap",
+      fontWeight: "medium",
+      display: "inline-flex",
+      justifyContent: "center",
+    },
+    icon: {
+      display: "flex",
+      marginRight: 2,
+    },
+  },
+  variants: {
+    size: {
+      sm: {
+        root: {
+          px: 7,
+          py: 2,
+          fontSize: "xs",
+        },
+        icon: {
+          width: 4,
+        },
+      },
+      md: {
+        root: {
+          px: 8,
+          py: 3,
+          fontSize: "sm",
+        },
+        icon: {
+          width: 5,
+        },
+      },
+      lg: {
+        root: {
+          px: 12,
+          py: 4,
+        },
+        icon: {
+          width: 6,
+        },
+      },
+    },
+    variant: {
+      "primary-solid": {
+        root: {
+          color: "white",
+          background: "linear-gradient(#ff5f5f, #f062c0)",
+          _hover: {
+            background: "linear-gradient(#e35565, #d958a6)",
+          },
+          _focus: {
+            outline: "3px solid rgba(242, 49, 165, 0.5)",
+          },
+        },
+      },
+      "primary-ghost": {
+        root: {
+          color: "wg_pink",
+          backgroundColor: "transparent",
+          _hover: {
+            backgroundColor: "stone.100",
+          },
+          _focus: {
+            outline: "3px solid rgba(242, 49, 165, 0.5)",
+          },
+        },
+      },
+      github: {
+        root: {
+          color: "white",
+          backgroundColor: "#24292E",
+          _hover: {
+            backgroundColor: "#191C20",
+          },
+          _focus: {
+            outline: "3px solid rgba(79, 83, 87, 0.5)",
+          },
+        },
+      },
+    },
+  },
+  defaultVariants: {
+    size: "md",
+    variant: "primary-solid",
+  },
+});
